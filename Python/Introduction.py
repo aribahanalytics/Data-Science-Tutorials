ba1f665ae6d076ea73da8d847ae80c76ba88294a
aribahanalytics <32316212+aribahanalytics@users.noreply.github.com>
2017-12-20 22:47:45 +0530

    First commit

---
+++ Python/Introduction.py
@@ -0,0 +1,34 @@
+#Python basics and advanced
+
+#Pandas
+
+- Read data from different data sources (Excel, csv, DB, any other data store )
+- Manipulate data
+- Apply different transformations
+
+#Matplotlib
+
+- Plotting library
+- different charts
+
+#Sklearn
+
+- Machine learning Library 
+- Can be used for text, Image, Tabular
+- Linear and Non -linear ML algo
+
+#Numpy
+
+#Spark
+
+- Scala 
+- Real time cluster setup
+- 20 Node cluster setup
+
+#Hive, Elastic Map Reduce, Amazon Web Services, Hadoop
+
+#MlSpark
+
+#Deep Learning
+
+#Tensorflow and Keras
